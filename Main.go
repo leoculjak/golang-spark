89771b4ddcb3350bae9b4b1c04f5dabd5e8a7b5d
leo <leo.culjak@edu.fit.ba>
2018-01-31 01:37:04 +0100

    first commit

---
+++ Main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", Index).Methods("GET")
+	router.HandleFunc("/store", PersonStore).Methods("POST")
+	router.HandleFunc("/update/{id}", PersonUpdate).Methods("PUT")
+	router.HandleFunc("/destroy/{id}", PersonDelete).Methods("DELETE")
+	router.HandleFunc("/show/{id}", PersonShow).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// Person Data model
+type Person struct {
+	Id          int    `json:id`
+	Firstname   string `json:firstname`
+	Lastname    string `json:lastname`
+	Email       string `json:email`
+	Phonenumber string `json:phonenumber`
+	Birth       string `json:birth`
+}
+
+// Index Index page
+func Index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, "SPARK!\n")
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/spark")
+	checkErr(err)
+
+	rows, err := db.Query("SELECT * FROM person")
+	checkErr(err)
+
+	var p Person
+	var ppl []Person
+
+	for rows.Next() {
+		err = rows.Scan(&p.Id, &p.Firstname, &p.Lastname, &p.Email, &p.Phonenumber, &p.Birth)
+		checkErr(err)
+		ppl = append(ppl, p)
+		// json.NewEncoder(w).Encode(p)
+	}
+
+	json.NewEncoder(w).Encode(ppl)
+
+	db.Close()
+}
+
+// PersonStore STORE
+func PersonStore(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var p Person
+	err := decoder.Decode(&p)
+	checkErr(err)
+	defer r.Body.Close()
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/spark")
+	checkErr(err)
+
+	stmt, err := db.Prepare("INSERT INTO person(firstname, lastname, email, phonenumber, birth) VALUES(?,?,?,?,?)")
+	checkErr(err)
+
+	res, err := stmt.Exec(p.Firstname, p.Lastname, p.Email, p.Phonenumber, p.Birth)
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Fprintln(w, id)
+
+	db.Close()
+}
+
+// PersonUpdate UPDATE
+func PersonUpdate(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var p Person
+	err := decoder.Decode(&p)
+	checkErr(err)
+	defer r.Body.Close()
+	vars := mux.Vars(r)
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/spark")
+	checkErr(err)
+
+	stmt, err := db.Prepare("UPDATE person SET firstname=?, lastname=?, email=?, phonenumber=?, birth=? WHERE id=?")
+	checkErr(err)
+
+	res, err := stmt.Exec(p.Firstname, p.Lastname, p.Email, p.Phonenumber, p.Birth, vars["id"])
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+}
+
+// PersonShow SHOW
+func PersonShow(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+
+	var person Person
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/spark")
+	checkErr(err)
+
+	rows, err := db.Query("SELECT * FROM person WHERE id=" + vars["id"])
+	checkErr(err)
+
+	for rows.Next() {
+		err = rows.Scan(&person.Id, &person.Firstname, &person.Lastname, &person.Email, &person.Phonenumber, &person.Birth)
+		checkErr(err)
+	}
+
+	j, _ := json.Marshal(person)
+	w.Write(j)
+}
+
+// PersonDelete DESTROY
+func PersonDelete(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/spark")
+	checkErr(err)
+
+	stmt, err := db.Prepare("DELETE FROM person WHERE id=?")
+	checkErr(err)
+
+	res, err := stmt.Exec(vars["id"])
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	db.Close()
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
